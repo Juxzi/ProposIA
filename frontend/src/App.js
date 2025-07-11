9705a74c50540285fd1760dc5e754c14c05d9d24
Juxzi <128641688+Juxzi@users.noreply.github.com>
2025-07-11 17:19:30 +0200

    Create responsive landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <h1>ProposIA Frontend</h1>
-    </div>
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
